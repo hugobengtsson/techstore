a1dbc4c124b4233c412fb7c8c35e0aa38fb61f7c
Hugo Bengtsson <hugo@capellium.se>
2021-10-12 10:54:56 +0200

    adding js load

--- main.js
+++ main.js
@@ -25,12 +25,51 @@ function addProductsToWebpage() {
     
     
 
-    for (let i = 0; i < listOfProducts.length; i++) {
-        const productList = listOfProducts[i];
-        
-        let title = document.createElement("title");
-        console.log(productList)
-    }
+    var imgSrc = "/assets/"
+    
+    
+    let main = document.getElementsByTagName("main")[0];
+    
+    listOfProducts.forEach((productList) => {
+
+
+        let title = listOfProducts.title
+
+        let productContainer = document.createElement("div")
+        productContainer.classList.add("productContainer")
+
+        let titleText = document.createElement("h2")
+        titleText.classList.add("titleText")
+        titleText.innerText = title
+
+        let description =  listOfProducts.description
+
+        let descriptionText = document.createElement("p")
+        descriptionText.classList.add("descriptionText")
+        descriptionText.innerText = description
+
+        let image = listOfProducts.image
+
+        let productImage = document.createElement("img")
+        productImage.src= imgSrc + image
+        productImage.classList.add("productImgage")
+
+        let price = listOfProducts.price
+        let productPrice = document.createElement("p")
+        productPrice.classList.add("productPrice")
+        productPrice.innerText = price + " kr"
+
+        let addToCartButton = document.createElement("button")
+        addToCartButton.classList.add("addToCartButton")
+        addToCartButton.innerHTML = "Lägg till i kundvagnen"
+
+
+
+        productContainer.append(titleText, descriptionText, productImage, productPrice, addToCartButton)
+        main.append(productContainer)
+
+
+    });
     // Add your code here, remember to brake your code in to smaller function blocks
     // to reduce complexity and increase readability. Each function should have
     // an explainetory comment like the one for this function, see row 22.
