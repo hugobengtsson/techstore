4d914fd9eff0dc7bb1262ef046555a7964b4af54
Hugo Bengtsson <hugo@capellium.se>
2021-10-18 12:59:29 +0200

    adding js for add to cart and showing cart quantitt

--- main.js
+++ main.js
@@ -12,20 +12,21 @@ function loadProducts() {
     });
 }
 
-
-function initSite() {
+let initSite = () => {
     loadProducts();
+    addQuantity()
     // This would also be a good place to initialize other parts of the UI
 }
 
 /** Uses the loaded products data to create a visible product list on the website */
 function addProductsToWebpage() {
     // Check your console to see that the products are stored in the listOfProducts varible.
-    document.getElementById("addToCartNumber").innerHTML =  localStorage.clickcount
     let imgSrc = "/assets/"
     let productContainer = document.getElementsByClassName("data-product-container")[0];
 
+
     listOfProducts.forEach((product) => {
+
 
     //Creating Conatiner
         let container = document.createElement('div')
@@ -68,26 +69,17 @@ function addProductsToWebpage() {
         productButtonText.classList.add('product-button-text')
         productButtonText.innerHTML = "Lägg till i kundvagnen"
 
-        productButtonContainer.addEventListener('click',function() {
-
-            if (typeof(Storage) !== "undefined") {
-                if (localStorage.clickcount) {
-                localStorage.clickcount = Number(localStorage.clickcount)+1;
-                } else {
-                localStorage.clickcount = 1;
-                }
-                document.getElementById("addToCartNumber").innerHTML =  localStorage.clickcount
-            } 
-            console.log(localStorage)
+        productButtonContainer.addEventListener('click', function() {
+            addToCart(product)
+            addQuantity()
         })
-
+        
         
         productItems.append(productButtonContainer)
         productButtonContainer.append(productButtonIcon,productButtonText)
-
+        
 
     });
-
 
     // Add your code here, remember to brake your code in to smaller function blocks
     // to reduce complexity and increase readability. Each function should have
@@ -95,3 +87,46 @@ function addProductsToWebpage() {
     
     // TODO: Remove the console.log and these comments when you've read them.
 }
+
+
+
+
+function addToCart(product){
+
+    let cartKey = localStorage.getItem("cart")
+    let cart = []
+
+    if(cartKey) {
+        cart = JSON.parse(cartKey)
+    }
+    let foundIndex = cart.findIndex(cartItem => cartItem.product.title == product.title)
+
+    if(foundIndex >= 0) {
+        Number(cart[foundIndex].quantity++)
+    } else {
+        cart.push({product: product, quantity: 1})
+    }
+
+    localStorage.setItem("cart", JSON.stringify(cart))
+
+}
+
+function addQuantity(){
+
+    let cart = JSON.parse(localStorage.getItem("cart"))
+
+    let quantity = 0
+    if (cart){
+
+        cart.forEach((product) => {
+
+        quantity = quantity + Number(product.quantity)
+
+        })
+    } else {
+
+    }
+    document.getElementById("addToCartNumber").innerText = quantity;
+}
+
+window.addEventListener("load", initSite)
