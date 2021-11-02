d080d6ac513c8e5d16fde00f4e0a18605315648e
Hugo Bengtsson <hugo@capellium.se>
2021-11-02 14:05:51 +0100

    adding comments and cleaning up code
    also fixed bug with login form

--- common.js
+++ common.js
@@ -1,199 +1,256 @@
+// Function to add quantity to the quantity number in the nav bar
 export function addQuantity(){
 
+    // Variable for cart from localStorage
     let cart = JSON.parse(localStorage.getItem("cart"))
 
+    // Variable for quantity counter
     let quantity = 0
+
+    // If cart exists in localStorage
     if(cart) {
 
+        // Looping all the cart items
         cart.forEach((product) => {
 
+        // Adding quantity togeather with every run of the loop
         quantity = quantity + Number(product.quantity)
 
         })
-    } else {
+    } 
 
-    }
+    // Setting the cart number to quantity
     document.getElementById("addToCartNumber").innerText = quantity;
 
 }
 
-// checks if the user is logged in, gets updated with initSite on load
+// Checks if the user is logged in, gets updated with initSite on load
 export function loggedIn(){
 
-    
+    // Checks if the loggedIn key exists in localStorage
     if(localStorage.getItem("loggedIn")){
 
+        // Variable for loggedIn key from localStorage
         let loggedInKey = JSON.parse(localStorage.getItem("loggedIn"))
 
+        // Checks if loggedin is is true
         if(loggedInKey.loggedIn == true){
-    
+
+            // Renders a log out button
             let loggOutButton = document.createElement("p")
             loggOutButton.classList.add("loggOutButton")
             loggOutButton.innerText = "Logga ut"
             document.getElementsByClassName("loginNavContainer")[0].append(loggOutButton)
-    
+
+            // Changes the account icon to green
             document.getElementsByClassName("loginSymbol")[0].style.color = "green"
         }
-
-
-
     }
-
 }
 
+// Function for opening the log in element
 function openLogInDiv(){
 
-
+    // Checks if loggedIn exists in localStorage
     if(localStorage.getItem("loggedIn")){
 
+        // Variable for loggedIn key from localStorage
         let loggedInKey = JSON.parse(localStorage.getItem("loggedIn"))
         
+        // Checks if loggedin is is true
         if(loggedInKey.loggedIn == true){
 
         }
         else{
 
+            // If the user is not logged in you are able to open the element to log in
             document.getElementsByClassName("loginContainer")[0].style.display = "flex"
 
         }
     }
     else{
-
+    // If loggedIn does not exist in localStorage you are able to log in
         document.getElementsByClassName("loginContainer")[0].style.display = "flex"
 
     }
-
-    /* document.getElementsByClassName("loginContainer")[0].addEventListener("click", function(event){
-            
-        alert(event.target.className)
-        //event.stopPropagation();   // W3C model
-        
-        if(event.target.className = "loginContainer flexContainer alignItems justifyContent"){
-            document.getElementsByClassName("loginContainer")[0].style.display = "none"
-        } 
-
-    }) */
-
 }
 
+// Function to close the log in element
 function closeLogInDiv(){
 
+    // If the close button is clicked, display = "none"
     document.getElementsByClassName("loginContainer")[0].style.display = "none"
 
 }
 
-// for registering accounts through the log in div
+// Function for registering accounts through the log in div
 export function registerAccount(event){
 
-    // prevents the site to reload on submitt of the form
+    // Prevents the default event from the log in form
     event.preventDefault()
 
+    // Variables for inputed username and password
     let createUsername = document.getElementById("createUsername").value
     let createPassword = document.getElementById("createPassword").value
 
+    // Variable for accounts in local storage
     let accountKey = JSON.parse(localStorage.getItem("accounts"))
 
+    // Variable for create acount
     let createAccount = []
 
+    // Checks if the account key exists in localStorage
     if(accountKey){
 
+        // Setting createAccount to the accounts in the accountKey from localStorage
         createAccount = accountKey
 
+        // Checks if the created username matches with any of the existing
         let foundUsername = accountKey.findIndex(account => account.username == createUsername)
 
+        // Checks if there was a match between the input username and any of the registed usernames
         if(foundUsername >= 0){
 
+            // Creates text for telling the user that the username exists
             let existingUsername = document.createElement("p")
             existingUsername.innerText = "Användarnamnet är upptaget"
 
+            // Resets the box for text and appends existingUsername
             document.getElementsByClassName("usernameExists")[1].innerText = ""
             document.getElementsByClassName("usernameExists")[1].append(existingUsername)
 
         }
+        // If the username is not matched
         else{
 
+            // Adding created username and password to list as an object
             createAccount.push({username: createUsername, password: createPassword})
+
+            // Setting accounts to localStorage
             localStorage.setItem("accounts", JSON.stringify(createAccount))
+
+            // Setting localStorage loggedIn to true
             localStorage.setItem("loggedIn", JSON.stringify({loggedIn: true, username: createUsername}))
+
+            // Reloading the site
             location.reload()
 
         }
     }
+    // If the account key in localStorage does not exist
     else{
 
+        // Adding created username and password to list as an object
         createAccount.push({username: createUsername, password: createPassword})
+
+        // Setting accounts to localStorage
         localStorage.setItem("accounts", JSON.stringify(createAccount))
+
+        // Setting localStorage loggedIn to true
         localStorage.setItem("loggedIn", JSON.stringify({loggedIn: true, username: createUsername}))
+
+        // Reloading the site
         location.reload()
     }
 }
 
-// logs users in through the log in div
+// Function for logging in through the log in div and form
 function logIn(event){
 
-    // prevents the site to reload on submitt of the form
+    // Prevents the default event from the log in form
     event.preventDefault()
 
+    // Variables for inputed username and password
     let username = document.getElementById("username").value
     let password = document.getElementById("password").value
 
+    // Variable for accounts in local storage
     let accountKey = JSON.parse(localStorage.getItem("accounts"))
 
+    // Checks if the account key exists in localStorage
     if(accountKey){
 
+        // Checks if the created username matches with any of the existing
         let foundUser = accountKey.findIndex(account => account.username == username)
 
+        // Checks if there is a match
         if(foundUser >= 0){
 
+            // If the username is matching we check the password
             if(accountKey[foundUser].password == password){
 
+                // Setting localStorage loggedIn to true
                 localStorage.setItem("loggedIn", JSON.stringify({loggedIn: true, username: username}))
+
+                // Reloading the site
                 location.reload()
 
             }
+            // If the password do not match
             else{
 
+                // Creates text for telling the user that the username or password is incorrect
                 let existingUsername = document.createElement("p")
                 existingUsername.innerText = "Fel användarnamn eller lösenord"
-    
+
+                // Resets the box for text and appends existingUsername
                 document.getElementsByClassName("usernameExists")[0].innerText = ""
                 document.getElementsByClassName("usernameExists")[0].append(existingUsername)                
             }
-            }
+        }
+        // If the username does not match with the ones in localStorage
+        else{
+
+            // Creates text for telling the user that the username or password is incorrect
+            let existingUsername = document.createElement("p")
+            existingUsername.innerText = "Fel användarnamn eller lösenord"
+
+            // Resets the box for text and appends existingUsername
+            document.getElementsByClassName("usernameExists")[0].innerText = ""
+            document.getElementsByClassName("usernameExists")[0].append(existingUsername) 
+        }
     }
     else{
+
+        // Creates text for telling the user that the username or password is incorrect
         let existingUsername = document.createElement("p")
         existingUsername.innerText = "Fel användarnamn eller lösenord"
 
+        // Resets the box for text and appends existingUsername
         document.getElementsByClassName("usernameExists")[0].innerText = ""
         document.getElementsByClassName("usernameExists")[0].append(existingUsername)
     }
 }
 
-// logs out the user on click from eventlistener
+// Function for logging out
 function logOut(){
 
+    // Sets loggedIn to false
     localStorage.setItem("loggedIn", JSON.stringify({loggedIn: false, username: ""}))
 
+    // Reloading the site
     location.reload()
 
 }
 
-
+// Function for all the eventListeners
 export function eventListeners() {
 
+    // Eventlistener for submitting the logIn form
     document.getElementsByClassName("login")[0].addEventListener("submit", function(event){
         
         logIn(event)
     
     })
 
+    // Eventlistener for sumbitting the createAcount form
     document.getElementsByClassName("createAcount")[0].addEventListener("submit", function(event){
 
         registerAccount(event)
 
     })
 
+    // If the logout button exists, eventlistener runs the function for logging out
     if(document.getElementsByClassName("loggOutButton")[0]){
 
         document.getElementsByClassName("loggOutButton")[0].addEventListener("click", function(){
@@ -204,12 +261,14 @@ export function eventListeners() {
 
     }
 
+    // Eventlistener for the account icon to run the function to open the log in div
     document.getElementsByClassName("loginSymbol")[0].addEventListener("click", function(){
 
         openLogInDiv()
 
     })
 
+    // Eventlistener for the close icon in the log in div
     document.getElementsByClassName("closeIcon")[0].addEventListener("click", function(){
 
         closeLogInDiv()
--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Importing functions from common.js
 import { addQuantity } from './common.js'
 import { eventListeners } from './common.js'
 import { loggedIn } from './common.js'
@@ -18,6 +19,7 @@ function loadProducts() {
     });
 }
 
+// Runs all the functions to load the site
 let initSite = () => {
     loadProducts()
     addQuantity()
@@ -27,64 +29,69 @@ let initSite = () => {
 
 /** Uses the loaded products data to create a visible product list on the website */
 function addProductsToWebpage() {
-    
+
+    // Variable for easier access to image src
     let imgSrc = "/assets/"
+
+    // Container for rendering products
     let productContainer = document.getElementsByClassName("data-product-container")[0];
 
 
+    // Looping out products from the JSON-file
     listOfProducts.forEach((product) => {
 
 
-    //Creating Conatiner
-    // Bör tas bort vid version 2.0..
+        // Creating Conatiner
         let container = document.createElement('div')
         container.classList.add('product-container')
         productContainer.append(container)
 
-    //creating product-items inside my container
+        // Creating product-items inside the container
         let productItems = document.createElement('div')
         productItems.classList.add('product-item')
         container.append(productItems)
 
-    //creating h1 inside productItems that is the title in the product
+        // Creating H1 inside productItems that is the title in the product
         let productTitle = document.createElement('h1')
         productTitle.innerText = product.title
         productItems.append(productTitle)
         
-    //creating description inside productItems that is the description in the product
+        // Creating description inside productItems that is the description in the product
         let productDescription = document.createElement('h2')
         productDescription.innerText = product.description
         productItems.append(productDescription)
 
-    //creating img inside productItems that is the imgSrc + image (name of the image) in the product
+        // Creating img inside productItems that is the imgSrc + image (name of the image) in the product
         let productImage = document.createElement('img')
         productImage.src = imgSrc + product.image
         productItems.append(productImage)
 
-    //creating p inside productItems that is the price in the product
+        // Creating p inside productItems that is the price in the product
         let productPrice = document.createElement('p')
         productPrice.innerText = product.price + ' kr'
         productItems.append(productPrice)
 
-    //creating Button inside productItems that is the button for the product
+        // Creating button inside productItems that is the button for the product
         let productButtonContainer = document.createElement('div')
         productButtonContainer.classList.add('product-button-container')
         
+        // Creating cart-icon
         let productButtonIcon = document.createElement('i')
         productButtonIcon.classList.add('fas','fa-cart-arrow-down')
         
+        // Creating button-text
         let productButtonText = document.createElement('p')
         productButtonText.classList.add('product-button-text')
         productButtonText.innerHTML = "Lägg till i kundvagnen"
 
+        // EventListener for onClick to the add to cart button, also runs addQuantity to update the quantity next to the cart icon in the navbar 
         productButtonContainer.addEventListener('click', function() {
             addToCart(product)
             addQuantity()
         })
         
-        
-        productItems.append(productButtonContainer)
         productButtonContainer.append(productButtonIcon,productButtonText)
+        productItems.append(productButtonContainer)
         
 
     });
@@ -93,25 +100,34 @@ function addProductsToWebpage() {
 
 
 
-
+// Function to add products to the cart
 function addToCart(product){
 
     let cartKey = localStorage.getItem("cart")
     let cart = []
 
+    // Checks if the key for cart exists in localStorage
     if(cartKey) {
+        // If the key exists it is parsed from localStorage and added to the cart variable
         cart = JSON.parse(cartKey)
     }
+
+    // Checks if the product added to the cart already exists in cart
     let foundIndex = cart.findIndex(cartItem => cartItem.product.title == product.title)
 
+    // Checks if the product was found in the findIndex above
     if(foundIndex >= 0) {
+        // If the product was found we add quantity to the product
         Number(cart[foundIndex].quantity++)
     } else {
+        // If the product did not already exist in cart, we add the product to cart and add quantity
         cart.push({product: product, quantity: 1})
     }
 
+    // Setting cart in localStorage. 
     localStorage.setItem("cart", JSON.stringify(cart))
 
 }
 
+// Eventlistener to run the site when it is loaded
 window.addEventListener("load", initSite)
